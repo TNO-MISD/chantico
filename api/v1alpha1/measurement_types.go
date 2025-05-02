9dd6e2b1845ea8d02fa32bc4899720edc26f65eb
Robin Camarasa <robin.camarasa@tno.nl>
2025-05-02 16:34:58 +0200

    Add basic prometheus measurement

---
+++ api/v1alpha1/measurement_types.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MeasurementSpec defines the desired state of Measurement
+type MeasurementSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Measurement. Edit measurement_types.go to remove/update
+	Name       string `json:"name"`
+	IsInternal bool   `json:"isInternal"`
+	Protocol   string `json:"protocol"`
+	DataSource string `json:"dataSource"`
+	Query      string `json:"query"`
+}
+
+// MeasurementStatus defines the observed state of Measurement
+type MeasurementStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Measurement is the Schema for the measurements API
+type Measurement struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MeasurementSpec   `json:"spec,omitempty"`
+	Status MeasurementStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// MeasurementList contains a list of Measurement
+type MeasurementList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Measurement `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Measurement{}, &MeasurementList{})
+}
--- cmd/main.go
+++ cmd/main.go
@@ -151,6 +151,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Pdu")
 		os.Exit(1)
 	}
+	if err = (&controller.MeasurementReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Measurement")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
