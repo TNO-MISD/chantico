2d808f81c8b86a92f5cab8e37de65826ec75ec48
Robin Camarasa <robin.camarasa@tno.nl>
2025-05-20 14:44:36 +0200

    Update the logic of the aggregator

--- chantico/aggregator/main.go
+++ chantico/aggregator/main.go
@@ -22,12 +22,10 @@ import (
 )
 
 type Args struct {
-	StartTimeArg     *string
 	DurationArg      *string
 	PostgresURL      *string
 	MeasurementIDArg *string
 
-	StartTime     time.Time
 	MeasurementID pgtype.UUID
 	Duration      time.Duration
 }
@@ -35,17 +33,11 @@ type Args struct {
 func (args *Args) Process() {
 	var err error
 
-	args.StartTimeArg = flag.String("time", "2006-01-02 15:04:05", "the start range of the query")
-	args.DurationArg = flag.String("duration", "59m", "the duration")
+	args.DurationArg = flag.String("duration", "1h", "the duration")
 	args.PostgresURL = flag.String("pgdbstring", os.Getenv("PG_DBSTRING"), "the url of the postgres database - the default value is the PG_DBSTRING env variable")
 	args.MeasurementIDArg = flag.String("uuid", "", "the measurement UUID")
 
 	flag.Parse()
-
-	args.StartTime, err = time.Parse("2006-01-02 15:04:05", *args.StartTimeArg)
-	if err != nil {
-		panic(err)
-	}
 
 	args.MeasurementID = *new(pgtype.UUID)
 	err = args.MeasurementID.Scan(*args.MeasurementIDArg)
@@ -57,10 +49,113 @@ func (args *Args) Process() {
 	if err != nil {
 		panic(err)
 	}
+}
 
-	if args.StartTime.Add(args.Duration).Unix() > time.Now().Unix() {
-		log.Println("This end time range is in the future.")
-		os.Exit(0)
+var retentionDuration time.Duration = (5 * time.Hour)
+
+func UpdateMeasurement(measurement sqlhelper.Measurement, args Args, ctx context.Context, db sqlhelper.DBTX) error {
+	queries := sqlhelper.New(db)
+
+	// Get measurement by id
+	measurement, err := queries.GetMeasurement(ctx, args.MeasurementID)
+	if err != nil {
+		fmt.Println("Could not create measurement", err)
+		return err
+	}
+
+	// Add logic about to determine the start and end time
+	maxEndTime := time.Now().UTC()
+	minStartTime := maxEndTime.Add(-retentionDuration).Round(time.Hour)
+	if minStartTime.Before(measurement.RegistrationTime.Time) {
+		minStartTime = minStartTime.Add(time.Hour)
+	}
+
+	startTime := measurement.RegistrationTime.Time.Round(time.Hour)
+	if startTime.Before(measurement.RegistrationTime.Time) {
+		startTime = startTime.Add(time.Hour)
+	}
+	if measurement.LastMeasurementTime.Time.After(startTime) {
+		startTime = measurement.LastMeasurementTime.Time
+	}
+	if startTime.Before(minStartTime) {
+		startTime = minStartTime
+	}
+
+	for {
+		endTime := startTime.Add(args.Duration).Add(-time.Second)
+		if endTime.After(maxEndTime) {
+			log.Printf("No more measurement, gracefully shutting down")
+			os.Exit(0)
+		}
+
+		// Get the prometheus data
+		query_url := fmt.Sprintf(
+			"http://%s/api/v1/query_range?query=%s&step=1&start=%d.000&end=%d.000",
+			measurement.DataSource,
+			url.PathEscape(measurement.Query),
+			startTime.UTC().Unix(),
+			endTime.UTC().Unix(),
+		)
+
+		resp, err := http.Get(query_url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		requestResponse := &prometheus.PrometheusRequestResponse{}
+		requestResponse.Parse(body)
+
+		var acc int64 = 0
+		n := 0
+		for _, value := range (requestResponse.Data.Result[0]).Values {
+			if len(value) != 2 {
+				log.Fatal("Not enough values")
+			}
+			valueString := value[1]
+			valueInt, err := strconv.ParseInt(valueString, 10, 64)
+			if err != nil {
+				log.Fatal("values wrongly formatted")
+			}
+			acc += valueInt
+			n++
+		}
+
+		// Push the data to Postgres
+		startTimestamp := pgtype.Timestamp{}
+		err = startTimestamp.Scan(startTime)
+		if err != nil {
+			panic("Start time wrongly formatted")
+		}
+
+		endTimestamp := pgtype.Timestamp{}
+		err = endTimestamp.Scan(endTime)
+		if err != nil {
+			panic("Duration wrongly formatted")
+		}
+
+		measurementValueParams := sqlhelper.CreateMeasurementValueParams{
+			MeasurementID:  args.MeasurementID,
+			Value:          float32(acc) / float32(n),
+			TimestampStart: startTimestamp,
+			TimestampEnd:   endTimestamp,
+		}
+		measurementValue, err := queries.CreateMeasurementValue(ctx, measurementValueParams)
+
+		// Update last measurement
+		updateLastMeasurementParams := sqlhelper.UpdateLastMeasurementTimeParams{
+			ID:                  args.MeasurementID,
+			LastMeasurementTime: endTimestamp,
+		}
+		_, err = queries.UpdateLastMeasurementTime(ctx, updateLastMeasurementParams)
+
+		if err != nil {
+			panic("Could not update last measurement")
+		}
+
+		log.Printf("Created measurement: %s\n", measurementValue.ID)
+		startTime = endTime.Add(time.Second)
 	}
 }
 
@@ -83,62 +178,10 @@ func main() {
 	// Get measurement by id
 	measurement, err := queries.GetMeasurement(ctx, args.MeasurementID)
 	if err != nil {
-		fmt.Println("Could not create measurement", err)
-		return
+		log.Fatal(fmt.Sprintf("Could not create measurement: %s\n", err))
 	}
-
-	// Get the prometheus data
-	query_url := fmt.Sprintf(
-		"http://%s/api/v1/query_range?query=%s&step=1&start=%d.000&end=%d.000",
-		measurement.DataSource,
-		url.PathEscape(measurement.Query),
-		args.StartTime.Unix(),
-		args.StartTime.Add(args.Duration).Unix(),
-	)
-	resp, err := http.Get(query_url)
+	err = UpdateMeasurement(measurement, args, ctx, db)
 	if err != nil {
-		panic(err)
+		log.Fatal(fmt.Sprintf("Could not create measurement: %s\n", err))
 	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	requestResponse := &prometheus.PrometheusRequestResponse{}
-	requestResponse.Parse(body)
-
-	var acc int64 = 0
-	n := 0
-	for _, value := range (requestResponse.Data.Result[0]).Values {
-		if len(value) != 2 {
-			log.Fatal("Not enough values")
-		}
-		valueString := value[1]
-		valueInt, err := strconv.ParseInt(valueString, 10, 64)
-		if err != nil {
-			log.Fatal("values wrongly formatted")
-		}
-		acc += valueInt
-		n++
-	}
-
-	// Push the data to Postgres
-	startTimestamp := pgtype.Timestamp{}
-	err = startTimestamp.Scan(args.StartTime)
-	if err != nil {
-		panic("Start time wrongly formatted")
-	}
-
-	endTimestamp := pgtype.Timestamp{}
-	err = endTimestamp.Scan(args.StartTime.Add(args.Duration))
-	if err != nil {
-		panic("Duration wrongly formatted")
-	}
-
-	measurementValueParams := sqlhelper.CreateMeasurementValueParams{
-		MeasurementID:  args.MeasurementID,
-		Value:          float32(acc) / float32(n),
-		TimestampStart: startTimestamp,
-		TimestampEnd:   endTimestamp,
-	}
-	measurementValue, err := queries.CreateMeasurementValue(ctx, measurementValueParams)
-	fmt.Print("Created measurement: ", measurementValue.ID)
 }
