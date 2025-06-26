9c8f65ac36095fcaf617284ac96998a7f6f6ac89
RobinCamarasa <robin.camarasa@gmail.com>
2025-06-26 09:22:02 +0000

    Add nix flakes

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "MPLI documentation flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = builtins.listToAttrs (
+        map (
+          system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          {
+            name = system;
+            value.default = pkgs.mkShell {
+              packages = [
+                pkgs.go
+                pkgs.net-snmp
+                pkgs.gnumake
+                pkgs.plantuml
+                pkgs.goose
+                pkgs.sqlc
+              ];
+            };
+          }
+        ) systems
+      );
+    };
+}
